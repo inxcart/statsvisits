5703a5ba26ea81215a73f7b3357b30d5ec41ccf5
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:30:30 +0100

    Update statsvisits.php

--- statsvisits.php
+++ statsvisits.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class StatsVisits extends ModuleGraph
@@ -36,8 +36,8 @@ class StatsVisits extends ModuleGraph
 	{
 		$this->name = 'statsvisits';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.6.0';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		parent::__construct();
